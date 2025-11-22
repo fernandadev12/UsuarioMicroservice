eca2b45e964f62fd52a238b9cfc374942430a132
Fernanda Oliveira <fernandaos12@gmail.com>
2025-11-22 19:45:09 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,58 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UserMicroservice.Application.DTO;
+using UsersAPI.Interface;
+
+namespace UsuarioMicroservice.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly IUserService _usuarioService;
+
+        public UsuariosController(IUserService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        [HttpGet("me")]
+        public async Task<IActionResult> GetMe()
+        {
+            var usuarioId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (string.IsNullOrEmpty(usuarioId) || !Guid.TryParse(usuarioId, out var id))
+                return Unauthorized();
+
+            var usuario = await _usuarioService.GetUserById(id);
+
+            if (usuario == null)
+                return NotFound();
+
+            var usuarioDto = new UserDTO
+            {
+                Username = usuario.Username,
+                Password = usuario.Password
+            };
+
+            return Ok(usuarioDto);
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Administrador")]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var usuarios = await _usuarioService.GetAllUsers();
+                return Ok(usuarios);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { error = "Erro interno ao listar usuários", details = ex.Message });
+            }
+        }
+    }
+}
