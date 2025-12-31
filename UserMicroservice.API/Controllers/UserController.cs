faba2f43b6c54af522b1c75b97489dd3f63a453d
Fernanda Oliveira <fernandaos12@gmail.com>
2025-12-31 10:05:18 -0300

    documentacao swagger no controller usuario

--- UserMicroservice.API/Controllers/UserController.cs
+++ UserMicroservice.API/Controllers/UserController.cs
@@ -25,7 +25,11 @@ namespace UsuarioMicroservice.Controllers
             _mdtr = mdtr;
             _config = config;
         }
-
+        /// <summary>
+        /// Autentica o usuário e gera o token JWT
+        /// </summary>
+        /// <param name="loginDto">username e password</param>
+        /// <returns>bearer token</returns>
         [HttpPost("login")]
         [AllowAnonymous]
         public async Task<IActionResult> Login([FromBody] LoginUserDTO loginDto)
@@ -68,6 +72,10 @@ namespace UsuarioMicroservice.Controllers
             });
         }
 
+        /// <summary>
+        /// Retorna todos os usuários cadastrados acesso somente admin
+        /// </summary>
+        /// <returns>Todos usuários cadastrados</returns>
         [HttpGet("GetAllUsers")]
         [Authorize(Roles = "admin")]
         public async Task<IActionResult> GetAll()
@@ -76,6 +84,76 @@ namespace UsuarioMicroservice.Controllers
             return Ok(usuarios);
         }
 
+        /// <summary>
+        /// Retorna o usuário pelo username acesso somente admin
+        /// </summary>
+        /// <param name="username">username do usuario</param>
+        /// <returns>usuario cadastrado</returns>
+        [HttpGet("GetUserByUsername/{username}")]
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> GetUserByUsername(string username)
+        {
+            var usuarios = await _usuarioService.GetAllUserList();
+            return Ok(usuarios);
+        }
+
+        /// <summary>
+        /// Retorna o usuário pelo email acesso somente admin
+        /// </summary>
+        /// <param name="email">email do usuario</param>
+        /// <returns>usuario cadastrado</returns>
+        [HttpGet("GetUserByEmail/{email}")]
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> GetUserByEmail(string username)
+        {
+            var usuarios = await _usuarioService.GetAllUserList();
+            return Ok(usuarios);
+        }
+
+        /// <summary>
+        /// Deleta o usuário pelo id , acesso somente admin
+        /// </summary>
+        /// <param name="id">Id do usuario a ser deletado</param>
+        /// <returns>true para item deletado</returns>
+        [HttpGet("DeleteUser/{id}")]
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> DeleteUser(Guid id)
+        {
+            var usuarios = await _usuarioService.GetAllUserList();
+            return Ok(usuarios);
+        }
+
+        /// <summary>
+        /// Atualiza o usuário pelo id , acesso somente admin
+        /// </summary>
+        /// <param name="id">id do usuario</param>
+        /// <returns>true se atualizado com sucesso</returns>
+        [HttpGet("UpdateUser/{id}")]
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> UpdateUser(Guid id)
+        {
+            var usuarios = await _usuarioService.GetAllUserList();
+            return Ok(usuarios);
+        }
+
+        /// <summary>
+        /// Envia um email para o novo cadastrado ou login
+        /// </summary>
+        /// <param name="email">email a ser enviado com informação</param>
+        /// <returns>true se enviado com sucesso</returns>
+        [HttpGet("SendEmailNewRegisterOrLogin/{email}")]
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> SendEmailNewRegisterOrLogin(string email)
+        {
+            var usuarios = await _usuarioService.GetAllUserList();
+            return Ok(usuarios);
+        }
+
+        /// <summary>
+        /// Cadastra um novo usuário acesso admin e usuario 
+        /// </summary>
+        /// <param name="registerUserDTO">Dados do usuário</param>
+        /// <returns>objeto cadastrado com sucesso</returns>
         [HttpPost("register")]
         [AllowAnonymous]
         public async Task<IActionResult> Register([FromBody] RegisterUserDTO registerUserDTO)
